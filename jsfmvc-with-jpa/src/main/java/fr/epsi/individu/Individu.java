d9fb6875eb800f3b798fbce0d962030b4959e6be
kimsavinfo <kimsavinfo@gmail.com>
2014-06-21 22:16:30 +0200

    controller modifie

--- jsfmvc-with-jpa/src/main/java/fr/epsi/individu/Individu.java
+++ jsfmvc-with-jpa/src/main/java/fr/epsi/individu/Individu.java
@@ -1,20 +1,37 @@
 package fr.epsi.individu;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "individu")
 public class Individu {
 
+	@Id
+	@Column(name = "individuId")
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
+	@Basic
+	@Column(length = 30, nullable = false)
 	@Size(min = 1, max = 30, message = "Le nom est obligatoire est doit contenir au plus 30 caractères !")
 	private String nom;
 
+	@Basic
+	@Column(length = 30, nullable = false)
 	@Size(min = 1, max = 30, message = "Le prénom est obligatoire est doit contenir au plus 30 caractères !")
 	private String prenom;
 
+	@Column(length = 3, nullable = false)
 	@NotNull(message = "L'âge est obligatoire")
 	@Min(value = 0, message = "L'âge ne peut pas être négatif")
 	@Max(value = 150, message = "L'âge est incorrect")
--- jsfmvc-with-jpa/src/main/java/fr/epsi/individu/IndividuController.java
+++ jsfmvc-with-jpa/src/main/java/fr/epsi/individu/IndividuController.java
@@ -4,22 +4,32 @@ import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
 
 @Named
 @RequestScoped
 public class IndividuController {
 
 	private final Individu individu = new Individu();
+	private EntityManager entityManager;
 
 	public String create() {
 		return "individu?faces-redirect=true";
 	}
 
-	public void delete(long id) {
+	public void delete(long individuId) 
+	{
+		entityManager.createNativeQuery("delete from individu where individuId = ?")
+		.setParameter(1, individuId)
+		.executeUpdate();
 	}
 
 	public List<Individu> getAll() {
-		return null;
+		List<Individu> individus = null;
+		individus = entityManager.createQuery("select i from Individu i", Individu.class)
+				.getResultList();
+
+		return individus;
 	}
 
 	public Individu getIndividu() {
