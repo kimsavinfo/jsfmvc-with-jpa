0398d9aff57735967feffc5c3117b037b215e866
kimsavinfo <kimsavinfo@gmail.com>
2014-06-22 15:18:55 +0200

    erreur avax.el.ELException: Error reading 'individu' on type fr.epsi.individu.IndividuController--allOwbNormalScopeProxy0

--- jsfmvc-with-jpa/src/main/java/fr/epsi/individu/Individu.java
+++ jsfmvc-with-jpa/src/main/java/fr/epsi/individu/Individu.java
@@ -1,20 +1,34 @@
 package fr.epsi.individu;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "INDIVIDUS")
 public class Individu {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID_INDIVIDU")
 	private Long id;
 
+	@Column(name = "NOM_INDIVIDU")
 	@Size(min = 1, max = 30, message = "Le nom est obligatoire est doit contenir au plus 30 caractères !")
 	private String nom;
 
+	@Column(name = "PRENOM_INDIVIDU")
 	@Size(min = 1, max = 30, message = "Le prénom est obligatoire est doit contenir au plus 30 caractères !")
 	private String prenom;
 
+	@Column(name = "AGE_INDIVIDU")
 	@NotNull(message = "L'âge est obligatoire")
 	@Min(value = 0, message = "L'âge ne peut pas être négatif")
 	@Max(value = 150, message = "L'âge est incorrect")
--- jsfmvc-with-jpa/src/main/java/fr/epsi/individu/IndividuController.java
+++ jsfmvc-with-jpa/src/main/java/fr/epsi/individu/IndividuController.java
@@ -1,15 +1,18 @@
 package fr.epsi.individu;
 
+import java.util.Iterator;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
 
 @Named
 @RequestScoped
 public class IndividuController {
 
 	private final Individu individu = new Individu();
+	private EntityManager entityManager = EntityManagerUtil.getEntityManager();
 
 	public String create() {
 		return "individu?faces-redirect=true";
@@ -18,8 +21,29 @@ public class IndividuController {
 	public void delete(long id) {
 	}
 
-	public List<Individu> getAll() {
-		return null;
+	public List<Individu> getAll() 
+	{
+		System.out.println("GETALL");
+		
+		try 
+		{
+			entityManager.getTransaction().begin();
+			
+			@SuppressWarnings("unchecked")
+			List<Individu> individus = entityManager.createQuery("from Individu").getResultList();
+			for (Iterator<Individu> iterator = individus.iterator(); iterator.hasNext();) 
+			{
+				Individu individu = (Individu) iterator.next();
+			}
+			
+			entityManager.getTransaction().commit();
+			return individus;
+		} catch (Exception e) 
+		{
+			entityManager.getTransaction().rollback();
+			return null;
+		}
+		
 	}
 
 	public Individu getIndividu() {
