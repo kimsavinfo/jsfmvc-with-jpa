fa92199aba76b5da819254d29dfa5b958a0b5af3
kimsavinfo <kimsavinfo@gmail.com>
2014-06-22 14:44:54 +0200

    fresh start

--- jsfmvc-with-jpa/src/main/java/fr/epsi/individu/IndividuController.java
+++ jsfmvc-with-jpa/src/main/java/fr/epsi/individu/IndividuController.java
@@ -1,19 +1,9 @@
 package fr.epsi.individu;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.Resource;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceUnit;
-import javax.sql.DataSource;
 
 @Named
 @RequestScoped
@@ -21,65 +11,15 @@ public class IndividuController {
 
 	private final Individu individu = new Individu();
 
-	// @PersistenceContext(unitName = "Tutorial")
-	private EntityManager entityManager;
-	
-
-	// TEMPO, works
-	
-	@Resource(name = "jpa_ressources")
-	private DataSource dataSource;
-	 
-
-
 	public String create() {
 		return "individu?faces-redirect=true";
 	}
 
-	public void delete(long individuId) 
-	{
-		entityManager.createNativeQuery("delete from individu where individuId = ?")
-		.setParameter(1, individuId)
-		.executeUpdate();
+	public void delete(long id) {
 	}
 
 	public List<Individu> getAll() {
-		/* List<Individu> individus = null;
-		individus = entityManager.createQuery("select i from Individu i", Individu.class)
-				.getResultList();
-		 */
-
-		// TEMPO, works
-		
-		List<Individu> individus = new ArrayList<>();
-
-		try( java.sql.Connection connection = dataSource.getConnection() )
-		{
-			String request = "SELECT id_individu, nom_individu, prenom_individu, age_individu  FROM INDIVIDUS";
-
-			PreparedStatement pstmt = connection.prepareStatement(request);
-
-			try(ResultSet resultSet = pstmt.executeQuery())
-			{				
-				while(resultSet.next())
-				{
-					Individu individu = new Individu();
-					individu.setId(resultSet.getLong(1));
-					individu.setNom(resultSet.getString(2));
-					individu.setPrenom(resultSet.getString(3));
-					individu.setAge(resultSet.getInt(4));
-
-					individus.add(individu);
-				}
-			}
-		} 
-		catch (SQLException ex) 
-		{
-			ex.printStackTrace();
-		}
-		 
-
-		return individus;
+		return null;
 	}
 
 	public Individu getIndividu() {
