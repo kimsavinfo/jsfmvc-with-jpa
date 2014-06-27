37c42b0a79577e2a2f74d6419c4bfab5ebf9eb34
JohanHenin <johan.henin@gmail.com>
2014-06-27 19:11:17 +0200

    Revert "test-ejb"
    This reverts commit d44a96e45e8b839196d7160aedaa103447464738.
    
    Conflicts:
    	jsfmvc-with-jpa/src/main/java/fr/epsi/individu/Controleur.java

--- jsfmvc-with-jpa/src/main/java/fr/epsi/individu/IndividuController.java
+++ jsfmvc-with-jpa/src/main/java/fr/epsi/individu/IndividuController.java
@@ -2,10 +2,7 @@ package fr.epsi.individu;
 
 import java.util.List;
 
-import javax.ejb.*;
 import javax.annotation.Resource;
-import javax.ejb.TransactionManagement;
-import javax.enterprise.context.Dependent;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
@@ -17,41 +14,36 @@ import javax.transaction.RollbackException;
 import javax.transaction.SystemException;
 import javax.transaction.UserTransaction;
 
-
-@Stateless
-@Dependent
-//Il s'agit de la valeur par défaut
-@TransactionManagement(TransactionManagementType.BEAN)
-//Il s'agit de la valeur par défaut
-//@TransactionAttribute(TransactionAttributeType.REQUIRED)
+@Named
+@RequestScoped
 public class IndividuController {
 
-	//private Individu individu = new Individu();
+	private Individu individu = new Individu();
 	@PersistenceContext(unitName="individuPersistenceUnit")
 	private EntityManager entityManager;
 	@Resource
 	UserTransaction ut;
 
-	public String create(Individu individu) throws NotSupportedException, SystemException, IllegalStateException, SecurityException, HeuristicMixedException, HeuristicRollbackException, RollbackException 
+	public String create() throws NotSupportedException, SystemException, IllegalStateException, SecurityException, HeuristicMixedException, HeuristicRollbackException, RollbackException 
 	{
 		ut.begin();
-		/*boolean transactionOk = false;
+		boolean transactionOk = false;
 		try 
-		{*/
+		{
 			entityManager.persist(individu);
-		/*	transactionOk = true;
+			transactionOk = true;
 		}
 		finally 
 		{
 			if(transactionOk) 
-			{*/
-				ut.commit();/*
+			{
+				ut.commit();
 			}
 			else 
 			{
 				ut.rollback();
 			}
-		}*/
+		}
 
 		return "individu?faces-redirect=true";
 	}
@@ -60,25 +52,25 @@ public class IndividuController {
 	{
 		ut.begin();
 		boolean transactionOk = false;
-		/*try 
-		{*/
+		try 
+		{
 			entityManager.createNativeQuery("delete from INDIVIDUS where ID_INDIVIDU = ?")
 	            .setParameter(1, individuId)
 	            .executeUpdate();
 
-		/*	transactionOk = true;
+			transactionOk = true;
 		}
 		finally 
 		{
 			if(transactionOk) 
-			{*/
-				ut.commit();/*
+			{
+				ut.commit();
 			}
 			else 
 			{
 				ut.rollback();
 			}
-		}*/
+		}
 	}
 
 	public List<Individu> getAll() {
@@ -89,5 +81,7 @@ public class IndividuController {
 		return individus;
 	}
 
-	
+	public Individu getIndividu() {
+		return individu;
+	}
 }
